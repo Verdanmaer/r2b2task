11408c5c88009ee306a47e05576d1901ac318aca
Verdanmaer <verneradam618@gmail.com>
2022-05-23 20:55:24 +0200

    final push

--- backend/api/rows.php
+++ backend/api/rows.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-function fetchData(bool $dummyData = false): array
+function fetchData(bool $dummyData = false, string $pageNumber = '1'): array
 {
   if ($dummyData === true) {
 
@@ -29,16 +29,7 @@ function fetchData(bool $dummyData = false): array
     ];
   }
 
-  // if ($planets) {
-  //   $curl = curl_init('https://swapi.dev/api/planets/1');
-  //   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  //   $data = curl_exec($curl);
-
-  //   json_decode($data, true);
-  //   echo json_encode(['name' => $data['name'] ?? 0] ?? []);
-  // }
-
-  $curl = curl_init('https://swapi.dev/api/people');
+  $curl = curl_init('https://swapi.dev/api/people/?page=' . $pageNumber);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
   $data = curl_exec($curl);
   return json_decode($data, true);
@@ -48,6 +39,7 @@ function fetchData(bool $dummyData = false): array
 header('Content-Type: application/json; charset=utf-8');
 
 // HINT: you might want to setup CORS header
+header("Access-Control-Allow-Origin: *");
 
 // TASK: use query parameters `dummy` and pass it to fetchData function
 $dummyData = false;
@@ -55,7 +47,12 @@ if (isset($_GET['dummy'])) {
   $dummyData = true;
 }
 
-$data = fetchData($dummyData);
+$pageNumber = '1';
+if (isset($_GET['page'])) {
+  $pageNumber = $_GET['page'];
+}
+
+$data = fetchData($dummyData, $pageNumber);
 
 // HINT: set proper response code
 echo json_encode(['count' => $data['count'] ?? 0, 'results' => $data['results'] ?? []]);
